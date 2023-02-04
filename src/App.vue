681ecd102e71c619162cf0846e4705cbcee0d299
joaopaulofnmvc <103937437+joaopaulofnmvc@users.noreply.github.com>
2023-02-03 23:49:23 -0300

    refactor:ajustando responsividade

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-	<v-app>
-		<app-header />
-		<app-main />
-		<app-footer />
+  <v-app>
+    <app-header />
+    <app-main />
+    <app-footer />
 
-		<img class="icon" src="/static/icons/mario.png" alt="Icon" />
-		<img class="icon" src="/static/icons/estrela.png" alt="Icon" />
-		<img class="icon" src="/static/icons/link.png" alt="Icon" />
-		<img class="icon" src="/static/icons/vida.png" alt="Icon" />
-		<img class="icon" src="/static/icons/vida-verde.png" alt="Icon" />
-		<img class="icon" src="/static/icons/donkie.png" alt="Icon" />
-		<img class="icon" src="/static/icons/poke.png" alt="Icon" />
-	</v-app>
+    <img class="icon" src="/static/icons/mario.png" alt="Icon" />
+    <img class="icon" src="/static/icons/estrela.png" alt="Icon" />
+    <img class="icon" src="/static/icons/link.png" alt="Icon" />
+    <img class="icon" src="/static/icons/vida.png" alt="Icon" />
+    <img class="icon" src="/static/icons/vida-verde.png" alt="Icon" />
+    <img class="icon" src="/static/icons/donkie.png" alt="Icon" />
+    <img class="icon" src="/static/icons/poke.png" alt="Icon" />
+  </v-app>
 </template>
 
 <script>
@@ -20,81 +20,92 @@ import AppMain from "@/components/main/AppMain.vue";
 import AppFooter from "@/components/footer/AppFooter.vue";
 
 export default {
-	name: "App",
+  name: "App",
 
-	components: { AppHeader, AppMain, AppFooter },
+  components: { AppHeader, AppMain, AppFooter },
 };
 </script>
 
 <style lang="scss">
 .container {
-	max-width: 80%;
-	margin: 0 auto;
+  max-width: 80%;
+  margin: 0 auto;
 
-	@media (max-width: 400px) {
-		max-width: 95%;
-		margin: 0 auto;
-	}
-	@media (min-width: 1800px) {
-		max-width: 1600px;
-		margin: 0 auto;
-	}
+  @media (max-width: 400px) {
+    max-width: 95%;
+    margin: 0 auto;
+  }
+  @media (min-width: 1800px) {
+    max-width: 1600px;
+    margin: 0 auto;
+  }
 
-	@media (min-width: 2500px) {
-		max-width: 1800px;
-		margin: 0 auto;
-	}
+  @media (min-width: 2500px) {
+    max-width: 1800px;
+    margin: 0 auto;
+  }
 }
 .icon {
-	max-width: 60px;
-	min-height: 60px;
-	object-fit: cover;
-	position: absolute;
+  max-width: 60px;
+  min-height: 60px;
+  object-fit: cover;
+  position: absolute;
 
-	@media (max-width: 600px) {
-		max-width: 40px;
-		min-height: 40px;
-	}
+  @media (max-width: 600px) {
+    max-width: 40px;
+    min-height: 40px;
+  }
 }
 .icon:nth-of-type(1) {
-	top: 20px;
-	left: 80px;
+  top: 20px;
+  left: 80px;
 
-	@media (max-width: 600px) {
-		left: 10px;
-	}
+  @media (max-width: 600px) {
+    left: 10px;
+  }
 }
 .icon:nth-of-type(2) {
-	top: 50%;
-	right: 50px;
+  top: 50%;
+  right: 50px;
+  @media (max-width: 600px) {
+    top: 51%;
+  }
 }
 .icon:nth-of-type(3) {
-	bottom: 20px;
-	left: 20px;
+  bottom: 20px;
+  left: 20px;
 }
 .icon:nth-of-type(4) {
-	top: 25%;
-	right: 20px;
+  top: 25%;
+  right: 20px;
 }
 .icon:nth-of-type(5) {
-	top: 65%;
-	left: 20px;
+  top: 65%;
+  left: 20px;
+
+  @media (max-width: 500px) {
+    top: 58%;
+  }
 }
 
 .icon:nth-of-type(6) {
-	top: 35%;
-	left: 20px;
+  top: 35%;
+  left: 20px;
+
+  @media (max-width: 500px) {
+    top: 32%;
+  }
 }
 .icon:nth-of-type(7) {
-	bottom: 10%;
-	right: 20px;
+  bottom: 10%;
+  right: 20px;
 }
 
 .base-card {
-	border-radius: $border-radius;
-	color: $gray-10;
-	background-color: $white-1;
-	padding: 20px;
+  border-radius: $border-radius;
+  color: $gray-10;
+  background-color: $white-1;
+  padding: 20px;
 }
 
 @media (max-width: 400px) {
--- src/components/header/AppHeader.vue
+++ src/components/header/AppHeader.vue
@@ -1,50 +1,59 @@
 <template>
-	<header class="header container">
-		<v-row class="d-flex justify-space-between">
-			<v-col md="6" class="d-flex justify-center align-center">
-				<div>
-					<h1 data-aos="zoom-in" data-aos-duration="1000" class="header__title">Olá ,Sou<br />João Paulo</h1>
-					<h4 class="mb-3" data-aos="zoom-in" data-aos-delay="500" data-aos-duration="1000">Desenvolvedor Front-end</h4>
-					<div data-aos="zoom-in" data-aos-duration="1000" class="header__socials" title="Redes Sociais">
-						<a
-							target="_blank"
-							href="https://linkedin.com/in/joão-paulo-8b38b8254"
-							aria-label="Linkedin"
-							title="Ícone Linkedin"
-						>
-							<linkedin-icon />
-						</a>
-						<a target="_blank" href="mailto:joaopauloneto3687@gmail.com" aria-label="Email" title="Ícone Email">
-							<email-icon />
-						</a>
-						<a
-							target="_blank"
-							href="https://www.instagram.com/joao_paulo.png/"
-							aria-label="Instagram"
-							title="Ícone Instagram"
-						>
-							<instagram-icon />
-						</a>
-						<img class="header__socials-gif" src="/static/icons/mario.gif" alt="Icon" />
-					</div>
+  <header class="header container">
+    <v-row class="d-flex justify-space-between">
+      <v-col md="6" class="d-flex justify-center align-center">
+        <div>
+          <h1 data-aos="zoom-in" data-aos-duration="1000" class="header__title">Olá ,Sou<br />João Paulo</h1>
+          <h4 class="mb-3" data-aos="zoom-in" data-aos-delay="500" data-aos-duration="1000">Desenvolvedor Front-end</h4>
+          <div data-aos="zoom-in" data-aos-duration="1000" class="header__socials" title="Redes Sociais">
+            <a
+              target="_blank"
+              href="https://linkedin.com/in/joão-paulo-8b38b8254"
+              aria-label="Linkedin"
+              title="Ícone Linkedin"
+            >
+              <linkedin-icon />
+            </a>
+            <a target="_blank" href="mailto:joaopauloneto3687@gmail.com" aria-label="Email" title="Ícone Email">
+              <email-icon />
+            </a>
+            <a
+              target="_blank"
+              href="https://www.instagram.com/joao_paulo.png/"
+              aria-label="Instagram"
+              title="Ícone Instagram"
+            >
+              <instagram-icon />
+            </a>
+            <img class="header__socials-gif" src="/static/icons/mario.gif" alt="Icon" />
+          </div>
 
-					<v-btn class="mt-7" dark rounded color="#36B7FF" href="/static/arquivos/resume.pdf" target="_blank"
-						>Ver Resume</v-btn
-					>
-				</div>
-			</v-col>
-			<v-col cols="12" md="6" class="d-flex justify-center align-center">
-				<img
-					data-aos="zoom-in"
-					data-aos-delay="500"
-					data-aos-duration="1000"
-					class="header__avatar"
-					src="/static/foto.PNG"
-				/>
-			</v-col>
-		</v-row>
-		<div class="header__decorator"></div>
-	</header>
+          <v-btn
+            data-aos="zoom-in"
+            data-aos-delay="500"
+            data-aos-duration="1000"
+            class="mt-7"
+            dark
+            rounded
+            color="#36B7FF"
+            href="/static/arquivos/resume.pdf"
+            target="_blank"
+            >Ver Resume</v-btn
+          >
+        </div>
+      </v-col>
+      <v-col cols="12" md="6" class="d-flex justify-center align-center">
+        <img
+          data-aos="zoom-in"
+          data-aos-delay="500"
+          data-aos-duration="1000"
+          class="header__avatar"
+          src="/static/foto.PNG"
+        />
+      </v-col>
+    </v-row>
+    <div class="header__decorator"></div>
+  </header>
 </template>
 
 <script>
@@ -52,80 +61,80 @@ import EmailIcon from "../icons/EmailIcon.vue";
 import InstagramIcon from "../icons/InstagramIcon.vue";
 import LinkedinIcon from "../icons/LinkedinIcon.vue";
 export default {
-	name: "AppHeader",
-	components: { EmailIcon, InstagramIcon, LinkedinIcon },
+  name: "AppHeader",
+  components: { EmailIcon, InstagramIcon, LinkedinIcon },
 };
 </script>
 
 <style lang="scss" scoped>
 .header {
-	margin-top: 50px;
-	&__socials {
-		display: flex;
-		align-items: center;
-		gap: 20px;
+  margin-top: 50px;
+  &__socials {
+    display: flex;
+    align-items: center;
+    gap: 20px;
 
-		a {
-			display: flex;
-			align-items: center;
-		}
-	}
-	&__socials-gif {
-		max-width: 40px;
-		object-position: center;
-		margin-top: -15px;
-	}
-	&__title {
-		position: relative;
-		z-index: 1;
+    a {
+      display: flex;
+      align-items: center;
+    }
+  }
+  &__socials-gif {
+    max-width: 40px;
+    object-position: center;
+    margin-top: -15px;
+  }
+  &__title {
+    position: relative;
+    z-index: 1;
 
-		&::after {
-			content: "";
-			position: absolute;
-			top: 20px;
-			left: -10px;
-			background: url("/public/static/header-detail.svg");
-			width: 120px;
-			height: 120px;
-			background-repeat: no-repeat;
-			z-index: -1;
-		}
-	}
+    &::after {
+      content: "";
+      position: absolute;
+      top: 20px;
+      left: -10px;
+      background: url("/public/static/header-detail.svg");
+      width: 120px;
+      height: 120px;
+      background-repeat: no-repeat;
+      z-index: -1;
+    }
+  }
 
-	&__avatar {
-		object-position: center;
-		border-radius: 50%;
-		width: 280px;
-		height: 280px;
-		min-width: 280px;
-		min-height: 280px;
-	}
-	&__decorator {
-		margin-top: 20px;
-		width: 100%;
-		height: 4px;
-		background-color: $blue-5;
-	}
+  &__avatar {
+    object-position: center;
+    border-radius: 50%;
+    width: 280px;
+    height: 280px;
+    min-width: 280px;
+    min-height: 280px;
+  }
+  &__decorator {
+    margin-top: 20px;
+    width: 100%;
+    height: 4px;
+    background-color: $blue-5;
+  }
 
-	@media (max-width: 500px) {
-		&__title {
-			font-size: 3rem;
-		}
+  @media (max-width: 500px) {
+    &__title {
+      font-size: 3rem;
+    }
 
-		&__avatar {
-			width: 300px;
-			height: 300px;
-		}
-		&__decorator {
-			margin-top: 10px;
-			width: 100%;
-			height: 2px;
-			background-color: $blue-5;
-		}
+    &__avatar {
+      width: 300px;
+      height: 300px;
+    }
+    &__decorator {
+      margin-top: 10px;
+      width: 100%;
+      height: 2px;
+      background-color: $blue-5;
+    }
 
-		& {
-			margin-top: 10px;
-		}
-	}
+    & {
+      margin-top: 10px;
+    }
+  }
 }
 </style>
